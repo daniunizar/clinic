72a341413ff82b4738c1e8e06027fa9d1ae5dca5
Daniel Marcos <dmunoz@novadevs.com>
2025-12-09 17:50:35 +0100

    add receptionist: model, migration, factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Receptionist;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,9 +17,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Receptionist::factory()->create([
+            'name' => 'sample',
+            'email' => 'sample@mail.com',
+            'password' => 'Sample123!'
         ]);
     }
 }
