5527c75b77e84b824a6f35a8302e34a430b9f47a
Daniel Marcos <dmunoz@novadevs.com>
2025-12-09 18:42:16 +0100

    add treatments: model, migration, factory, seeder

---
+++ database/factories/TreatmentFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Treatment>
+ */
+class TreatmentFactory extends Factory
+{
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'label' => fake()->word(),
+            'price_in_cents' => fake()->numberBetween(10000, 900000),
+        ];
+    }
+}
---
+++ database/migrations/2025_12_09_183600_create_treatments_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('treatments', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('label');
+            $table->integer('price_in_cents');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('treatments');
+    }
+};
